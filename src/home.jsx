8ae91b28bb7290b7aa44b888646d0ba48a0b0a99
HK33M <112273747+hk33m@users.noreply.github.com>
2026-02-01 00:10:52 +0300

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -17,7 +17,7 @@ const [name,setname]=useState(()=>{
 const saved = localStorage.getItem("currentIndex");
 const chick = saved!==null ? parseInt(saved) : 0 ;
     return(
-        <div className="min-h-screen flex justify-center items-center bg-gray-100 dark:bg-slate-900 " dir="rtl">
+        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 dark:bg-slate-900 " dir="rtl">
              <div className="w-[400px] md:w-[600px] bg-white dark:bg-slate-800 shadow-2xl p-8 rounded-2xl text-center">
          <AnimatePresence>
                      <motion.div
@@ -56,7 +56,7 @@ const chick = saved!==null ? parseInt(saved) : 0 ;
         </motion.div>
         </AnimatePresence>
         </div>
-
+   <div className="text-center  p-3">جميع الحقوق محفوضة لدى المعلمة / رقية حسين حامظي <span className="text-[20px]">©</span> {new Date().getFullYear()}</div>
         </div>
 
     )
